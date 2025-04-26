a5a053ed8b99a3a56b87e914ed474a7080e1b5a1
hogiabao <hogiabao7725@gmail.com>
2025-04-27 00:31:52 +0700

    Add DotenvInitializer config

---
+++ src/main/java/com/hgb7725/blog/config/DotenvInitializer.java
@@ -0,0 +1,21 @@
+package com.hgb7725.blog.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DotenvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        Dotenv dotenv = Dotenv.load();
+        Map<String, Object> props = new HashMap<>();
+        dotenv.entries().forEach(entry -> props.put(entry.getKey(), entry.getValue()));
+        applicationContext.getEnvironment()
+                .getPropertySources()
+                .addFirst(new MapPropertySource("dotenv", props));
+    }
+}
